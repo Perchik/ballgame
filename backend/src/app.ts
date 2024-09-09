9d7386e3218bb9d30a5a5ad639dd5e5920a39cad
Patrick Dukes <pdukes3@gmail.com>
2024-09-09 11:57:39 -0400

    Add cors to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectToDatabase } from "./utils/mongodb";
 import gameRoutes from "./routes/game";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 // Middleware to parse JSON
 app.use(express.json());
 
